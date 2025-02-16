9b8eaaee5868a49e184d13adfd686d6f3a6a2be5
OleksandrLukomskyi <LUKOMSKYI.OLEKSANDR@GMAIL.COM>
2025-02-16 23:53:01 +0100

    add project

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,37 +1,66 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import { useId } from "react";
+import * as Yup from "yup";
+import css from "./UserForm.module.css";
 
-export default function UserForm({onSubmit}) {
-   const usernameFieldId = useId()
-   const roleFieldId = useId();
-   
-   const handleSubmit = (e) => {
-      e.preventDefault();
+const UserSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(3, "Too short!")
+    .max(20, "Too long!")
+    .required("This is required you dummy!"),
+  email: Yup.string()
+    .email("Must be in email format")
+    .required("This is required you dummy!"),
+  role: Yup.string().oneOf(["guest", "user", "admin"]).required(),
+});
 
-      onSubmit({
-         username: e.target.elements.username.value,
-         role: e.target.elements.role.value,
-      })
+export default function UserForm({ onFormSubmit }) {
+  const usernameFieldId = useId();
+  const emailField = useId();
+  const roleFieldId = useId();
 
-  e.target.reset();
+  return (
+    <Formik
+      initialValues={{
+        username: "",
+        email: "",
+        role: "user",
+      }}
+      validationSchema={UserSchema}
+      onSubmit={(values, actions) => {
+        console.log(values);
+        onFormSubmit(values);
+        actions.resetForm();
+      }}
+    >
+      <Form>
+        <div>
+          <label htmlFor={usernameFieldId}>Name:</label>
+          <Field type="text" name="username" id={usernameFieldId} />
+          <ErrorMessage
+            className={css.error}
+            name="username"
+            component="span"
+          />
+        </div>
 
-   }
+        <div>
+          <label htmlFor={emailField}>Email:</label>
+          <Field type="email" name="email" id={emailField} />
+          <ErrorMessage className={css.error} name="email" component="span" />
+        </div>
 
-   return(
-      
-<form onSubmit={handleSubmit}>
-        <label htmlFor={usernameFieldId}/>
-        <input type="text" name="username" id={usernameFieldId}/>
-       
-       
-       <label htmlFor={roleFieldId}></label>
-        <select name='role' id={roleFieldId}>
-          <option value="guest">Guest</option>
-          <option value="user">User</option>
-          <option value="admin">Admin</option>
-        </select>
+        <div>
+          <label htmlFor={roleFieldId}>Role:</label>
+          <Field as="select" name="role" id={roleFieldId}>
+            <option value="guest">Guest</option>
+            <option value="user">User</option>
+            <option value="admin">Admin</option>
+          </Field>
+          <ErrorMessage className={css.error} name="role" component="span" />
+        </div>
         <button type="submit">Submit</button>
-      </form>
-
-      
-   )
+      </Form>
+    </Formik>
+  );
 }
